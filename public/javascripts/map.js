b24677e6a9667b028a7c88b0510919fff5c9b146
diego acosta <you@example.com>
2020-09-08 14:23:28 -0300

    Pagina principal

---
+++ public/javascripts/map.js
@@ -0,0 +1,7 @@
+var map = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map);
