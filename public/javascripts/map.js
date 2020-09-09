7bc12355e103ebceac92a4e722a5666bc6b0e6ca
diego acosta <you@example.com>
2020-09-09 13:23:27 -0300

    Fixed map

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,7 +1,12 @@
-var map = L.map('main_map').setView([-34.59365, -58.450012], 13);
+var map = L.map('main_map', {
+    center: [-34.59365, -58.450012],
+    zoom: 13
+});
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+}).addTo(map)
 
-L.marker([-34.59365, -58.450012]).addTo(map);
+L.marker([-34.59865, -58.457652]).addTo(map);
+L.marker([-34.59952, -58.455822]).addTo(map);
+L.marker([-34.59634, -58.452192]).addTo(map);
